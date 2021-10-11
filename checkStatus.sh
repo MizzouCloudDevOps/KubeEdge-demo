33fab5ac64816f16f8752d3ea91c66b7c1975087
wangso <wangso@missouri.edu>
2021-10-10 22:16:11 -0500

    update

--- checkStatus.sh
+++ checkStatus.sh
@@ -4,9 +4,8 @@
 
 containerID=$(docker ps -a -q -f status=running -f "name=fastqc"  -f ancestor=mizzouceri/fastqc_wf:V1)
 
-job_id=$(docker logs $containerID | grep " pegasus-status -l " | cut -d' ' -f5)
+job_id=$(docker logs $containerID | grep " pegasus-status -l " | cut -d' ' -f5) >/dev/null 2>&1
 
-docker exec $containerID bash -c "pegasus-statistics $job_id -s all"
+docker exec $containerID bash -c "pegasus-statistics $job_id -s all" >/dev/null 2>&1
 
 summary=$(echo $job_id | cut -d'/' -f5,6) && grep 'Workflow wall time' /output/$summary/statistics/summary.txt
-
